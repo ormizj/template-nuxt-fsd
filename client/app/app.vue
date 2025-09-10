08448ec14098b192c119b56a2acef2bd9a14fd61
orm <orm@coperato.com>
2025-09-10 15:24:32 +0300

    master routing wip

--- client/app/app.vue
+++ client/app/app.vue
@@ -3,15 +3,27 @@ console.log('Running "app.vue"')
 </script>
 
 <template>
-  <dl>
-    <NuxtRouteAnnouncer/>
-    <h1>App</h1>
-    <NuxtLayout>
-      <NuxtPage/>
-    </NuxtLayout>
-  </dl>
+  <div id="app">
+    <dl>
+      <NuxtRouteAnnouncer/>
+      <h1>App</h1>
+      <NuxtLayout>
+        <NuxtPage/>
+      </NuxtLayout>
+    </dl>
+  </div>
 </template>
 
 <style scoped>
+#app {
+  display: flex;
+  justify-content: center;
+}
+</style>
 
+<style>
+body {
+  background-color: rgb(25, 25, 25);
+  color: rgb(225, 225, 225);
+}
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: {enabled: true},
+    sourcemap: true,
     srcDir: 'client/app',
     dir: {
         pages: 'routes',
